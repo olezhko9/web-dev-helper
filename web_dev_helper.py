6fc3d9cbdc0c3588b2060a14c739d7b19563abd2
Oleg Naumov <oleg.naumov.98@mail.ru>
2018-12-23 01:13:57 +0300

    Popup window with static content

--- web_dev_helper.py
+++ web_dev_helper.py
@@ -1,7 +1,20 @@
 import sublime
+from sublime import Window
 import sublime_plugin
 
-class WebDevHelper(sublime_plugin.TextCommand):
+
+class WebDevHelperCommand(sublime_plugin.TextCommand):
+
+    def show_command_description(self, content):
+        self.view.show_popup(content=content,
+                             flags=sublime.COOPERATE_WITH_AUTO_COMPLETE,
+                             max_width=1500,
+                             max_height=100)
 
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        content = "<h4>Background</h4>" + \
+                  "<p>Универсальное свойство background позволяет установить одновременно несколько характеристик фона.</p>" + \
+                  "<p>Универсальное свойство background позволяет установить одновременно несколько характеристик фона.Универсальное свойство background позволяет установить одновременно несколько характеристик фона.</p>" + \
+                  "<p>Универсальное свойство background позволяет установить одновременно несколько характеристик фона.</p>"
+
+        self.show_command_description(content)
