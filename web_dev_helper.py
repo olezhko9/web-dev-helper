27d0bc20d70a75f4446bc79067cae0e5678b0c36
Oleg Naumov <oleg.naumov.98@mail.ru>
2018-12-23 15:58:36 +0300

    Now popup always shows in first column

--- web_dev_helper.py
+++ web_dev_helper.py
@@ -1,17 +1,29 @@
 import sublime
-from sublime import Window
 import sublime_plugin
 
 
-class WebDevHelperCommand(sublime_plugin.TextCommand):
+class WebDevHelperCommand(sublime_plugin.WindowCommand):
+
+    def __init__(self, window):
+        super().__init__(window)
+        self.view = self.window.active_view()
 
     def show_command_description(self, content):
-        self.view.show_popup(content=content,
-                             flags=sublime.COOPERATE_WITH_AUTO_COMPLETE,
-                             max_width=1500,
-                             max_height=100)
 
-    def run(self, edit):
+        cursor_text_point = self.view.sel()[0].end()
+        cursor_line = self.view.rowcol(cursor_text_point)[0]
+        cursor_zero_col = self.view.text_point(cursor_line, 0)
+        print(cursor_zero_col)
+
+        # end_pos = self.view.sel()
+        # line_content = self.view.substr(self.view.line(end_pos[0].end()))
+
+        self.view.show_popup(content=content,
+                        location=cursor_zero_col,
+                        max_width=1000,
+                        max_height=300)
+
+    def run(self):
         content = "<h4>Background</h4>" + \
                   "<p>Универсальное свойство background позволяет установить одновременно несколько характеристик фона.</p>" + \
                   "<p>Универсальное свойство background позволяет установить одновременно несколько характеристик фона.Универсальное свойство background позволяет установить одновременно несколько характеристик фона.</p>" + \
