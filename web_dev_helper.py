5daf047246c6bc59451b092a17c01bf030230ad8
Oleg Naumov <oleg.naumov.98@mail.ru>
2018-12-25 23:21:55 +0300

    request command description from developer.mozilla.org

--- web_dev_helper.py
+++ web_dev_helper.py
@@ -1,6 +1,17 @@
 import sublime
 import sublime_plugin
 import re
+import sys
+import os
+
+# path to python site-packages
+site_packages_full_path = \
+    sorted([path if re.search(r'[pP]ython\d{1,2}\-\d{1,2}\\$', path) else '' for path in os.environ.get('PATH')
+           .split(';')], key=lambda s: len(s))[-1]
+sys.path.insert(0, site_packages_full_path + 'Lib\site-packages')
+import requests
+from bs4 import BeautifulSoup
+
 
 class WebDevHelperCommand(sublime_plugin.WindowCommand):
 
@@ -12,6 +23,8 @@ class WebDevHelperCommand(sublime_plugin.WindowCommand):
         self.command_pos = None
 
     def get_command_type(self, command_position):
+        if command_position.begin() == command_position.end():
+            return None
         line = self.view.substr(self.view.line(command_position.end())).lower()
         if re.search(re.compile(self.command_text + ':'), line):
             return 'css'
@@ -23,12 +36,51 @@ class WebDevHelperCommand(sublime_plugin.WindowCommand):
             return None
 
     def get_command_text(self, command_position):
+        if command_position.begin() == command_position.end():
+            return None
         selected_text = self.view.substr(command_position).lower().strip(' ')
         selected_text = re.search(r'[a-z\-]+', selected_text)
         if selected_text:
             return selected_text.group(0)
         else:
             return None
+
+    def get_command_description(self, command_text, command_type):
+        command_description = "Похоже, здесь нечего показывать."
+
+        if command_text is None or command_type is None:
+            return command_description
+
+        mozila_catalog = {
+            'root': 'https://developer.mozilla.org/en-US/docs/Web/',
+            'css': ['CSS/'],
+            'html': ['HTML/Element/'],
+            'js': ['API/Document/', 'API/Element/', 'API/EventTarget/']
+        }
+        url_catalog = mozila_catalog
+
+        base_url = url_catalog.get('root')
+
+        for u in url_catalog.get(command_type):
+            url = base_url + u + command_text
+            print(url)
+            try:
+                response = requests.get(url)
+                bs = BeautifulSoup(response.text, features="html.parser")
+                try_count = 0
+                html_selector = '#wikiArticle > p'
+                is_description = -1
+                while is_description == -1 and try_count < 2:
+                    command_description = bs.select(html_selector)[try_count].getText().replace('<', '&lt;').replace(
+                        '>', '&gt;')
+                    is_description = command_description.lower().find(command_text)
+                    try_count += 1
+                if is_description != -1:
+                    break
+            except IndexError:
+                command_description = "Похоже, здесь нечего показывать..."
+
+        return command_description
 
     def show_command_description(self, content):
         cursor_text_point = self.view.sel()[0]
@@ -38,19 +90,13 @@ class WebDevHelperCommand(sublime_plugin.WindowCommand):
 
         self.view.show_popup(content=content,
                              location=cursor_zero_col,
-                             max_width=int(viewport_width),
+                             max_width=int(viewport_width / 2),
                              max_height=300)
 
     def run(self):
-        content = "<h4>Background</h4>" + \
-                  "<p>Универсальное свойство background позволяет установить одновременно несколько характеристик фона.</p>" + \
-                  "<p>Универсальное свойство background позволяет установить одновременно несколько характеристик фона.Универсальное свойство background позволяет установить одновременно несколько характеристик фона.</p>" + \
-                  "<p>Универсальное свойство background позволяет установить одновременно несколько характеристик фона.</p>"
-
         self.command_pos = self.view.sel()[0]
-        if self.command_pos.end() != self.command_pos.begin():
-            self.command_text = self.get_command_text(self.command_pos)
-            self.command_type = self.get_command_type(self.command_pos)
-            self.show_command_description(content)
-        else:
-            print('0' * 10)
+        self.command_text = self.get_command_text(self.command_pos)
+        self.command_type = self.get_command_type(self.command_pos)
+
+        content = self.get_command_description(self.command_text, self.command_type)
+        self.show_command_description(content)
